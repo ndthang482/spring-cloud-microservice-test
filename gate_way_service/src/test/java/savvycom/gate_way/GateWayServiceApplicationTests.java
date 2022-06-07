71830fcaefda3c5e73ec5283af175f6c692a509b
ndthang4820 <thangthontien2k@gmail.com>
2022-06-07 17:33:23 +0700

    spring cloud eureka

---
+++ gate_way_service/src/test/java/savvycom/gate_way/GateWayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package savvycom.gate_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
